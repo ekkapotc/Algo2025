70c1e303556bd10b89d608d807e225dbc7045c96
ekkapot.c@tggs.kmutnb.ac.th <ekkapot.c@tggs.kmutnb.ac.th>
2025-08-22 16:19:50 +0700

    heap

--- DataStructuresI/PriorityQueue/main.cpp
+++ DataStructuresI/PriorityQueue/main.cpp
@@ -4,7 +4,7 @@
 
 int main(){
   int arr [] = {4,8,2,0,9,5,12,32,20,18,6};
-  heap<int> heap_int{arr,10};
+  heap<int> heap_int{arr,11};
   std::cout << heap_int << std::endl;
   std::cout << "max = " << heap_int.max() << std::endl;
   return 0;
