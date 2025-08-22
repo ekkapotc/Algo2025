4fab9df9a7a23f445cb471c26be9f26b6a415c98
ekkapot.c@tggs.kmutnb.ac.th <ekkapot.c@tggs.kmutnb.ac.th>
2025-08-22 21:36:36 +0700

    clang-format

--- DataStructuresI/Stack/main.cpp
+++ DataStructuresI/Stack/main.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
-#include <tuple>
 #include <string>
+#include <tuple>
 #include <utility>
 
 #include "stack.hpp"
 
-int main(){
-  
+int main() {
+
   stack<int> st_int;
-  for(auto i{0} ; i<10 ; i++) st_int.push(i);
-  for(auto i{0} ; i<10 ; i++) std::cout << st_int.pop() << std::endl;
-  
-  stack<std::tuple<int,int>> st_tup;
-  for(auto i{0} ; i<10 ; i++) st_tup.push(std::make_tuple(i,2*i));
-  for(auto i{0} ; i<10 ; i++) {
-     std::tuple<int,int> t = std::move(st_tup.pop());
-     std::cout << "( " << std::get<0>(t) << " , " << std::get<1>(t) << ")" << std::endl;
+  for (auto i{0}; i < 10; i++)
+    st_int.push(i);
+  for (auto i{0}; i < 10; i++)
+    std::cout << st_int.pop() << std::endl;
+
+  stack<std::tuple<int, int>> st_tup;
+  for (auto i{0}; i < 10; i++)
+    st_tup.push(std::make_tuple(i, 2 * i));
+  for (auto i{0}; i < 10; i++) {
+    std::tuple<int, int> t = std::move(st_tup.pop());
+    std::cout << "( " << std::get<0>(t) << " , " << std::get<1>(t) << ")"
+              << std::endl;
   }
 
-  std::string dict [] = {"hello", "bye", "morning", "welcome", "cheers", "thanks"};
+  std::string dict[] = {"hello",   "bye",    "morning",
+                        "welcome", "cheers", "thanks"};
   stack<std::string> st_str;
-  for(auto i{0} ; i<6; i++) st_str.push(dict[i]);
-  for(auto i{0} ; i<6; i++) std::cout << st_str.pop() << std::endl;
-
+  for (auto i{0}; i < 6; i++)
+    st_str.push(dict[i]);
+  for (auto i{0}; i < 6; i++)
+    std::cout << st_str.pop() << std::endl;
 
   return 0;
 }
--- DataStructuresI/Stack/stack.hpp
+++ DataStructuresI/Stack/stack.hpp
@@ -2,75 +2,68 @@
 #define STACK_HPP_INC
 
 #include <cassert>
-#include <utility>
 #include <cstddef>
+#include <utility>
 
-template <typename T>
-class stack{
-  private:
-    T * m_data; 
-    size_t m_size;
-    size_t m_cap;
-    
-    void reallocate(){
-        size_t new_cap = 2*m_cap;
-        T * new_data = new T[new_cap];
-        for(auto i{0} ; i<m_size ; i++) new_data[i] = std::move(m_data[i]);
-        delete [] m_data;
-        m_data = new_data;
-        m_cap = new_cap;
-    }
-  
-  public:
-    stack(size_t cap=4):m_data{nullptr},
-                      m_size{0},
-                      m_cap{cap}
-    {
-       assert(cap>0);
-       m_data = new T[m_cap];
-    }
+template <typename T> class stack {
+private:
+  T *m_data;
+  size_t m_size;
+  size_t m_cap;
 
-    ~stack(){
-	delete[] m_data;
-    }
+  void reallocate() {
+    size_t new_cap = 2 * m_cap;
+    T *new_data = new T[new_cap];
+    for (size_t i{0}; i < m_size; i++)
+      new_data[i] = std::move(m_data[i]);
+    delete[] m_data;
+    m_data = new_data;
+    m_cap = new_cap;
+  }
 
-    bool empty() const{
-        return m_size==0;
-    }
+public:
+  stack(size_t cap = 4) : m_data{nullptr}, m_size{0}, m_cap{cap} {
+    assert(cap > 0);
+    m_data = new T[m_cap];
+  }
 
-    size_t size() const{
-        return m_size;
-    }
+  ~stack() { delete[] m_data; }
 
-    void push(const T & elem){
-       if(m_size==m_cap) reallocate();
-       m_data[m_size] = elem;
-       m_size++;
-    }
+  bool empty() const { return m_size == 0; }
 
-    void push(T && elem){
-       if(m_size==m_cap) reallocate();     
-       m_data[m_size] = std::move(elem);
-       m_size++;
-    }
+  size_t size() const { return m_size; }
 
-    template <typename S>
-    void push(S && elem){
-       if(m_size==m_cap) reallocate();
-       m_data[m_size] = std::forward<S>(elem);
-       m_size++;
-    }
- 
-    T & top() {
-      assert(m_size>0);
-      return m_data[m_size-1];
-    }
+  void push(const T &elem) {
+    if (m_size == m_cap)
+      reallocate();
+    m_data[m_size] = elem;
+    m_size++;
+  }
 
-    T pop(){
-      assert(m_size>0);
-      m_size--;
-      return std::move(m_data[m_size]);
-    }   
+  void push(T &&elem) {
+    if (m_size == m_cap)
+      reallocate();
+    m_data[m_size] = std::move(elem);
+    m_size++;
+  }
+
+  template <typename S> void push(S &&elem) {
+    if (m_size == m_cap)
+      reallocate();
+    m_data[m_size] = std::forward<S>(elem);
+    m_size++;
+  }
+
+  T &top() {
+    assert(m_size > 0);
+    return m_data[m_size - 1];
+  }
+
+  T pop() {
+    assert(m_size > 0);
+    m_size--;
+    return std::move(m_data[m_size]);
+  }
 };
 
 #endif
