564149ff63aa3718397ba12f494e52ef83558b0d
ekkapot.c@tggs.kmutnb.ac.th <ekkapot.c@tggs.kmutnb.ac.th>
2025-08-23 08:42:25 +0700

    add SNode

---
+++ DataStructures/SNode/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+#include "snode.hpp"
+
+int main(){
+  SNode<int> ni1{3};
+  SNode<int> ni2{4};
+  ni1.set_next(&ni2);
+
+  std::string s1{"world"};
+
+  SNode<std::string> ns1{"hello"};
+  SNode<std::string> ns2{s1};
+  ns1.set_next(&ns2);
+
+  return 0;
+}
---
+++ DataStructures/SNode/snode.hpp
@@ -0,0 +1,64 @@
+#ifndef NODE_HPP_INC
+#define NODE_HPP_INC
+
+#include <utility>
+#include <iostream>
+
+template <typename T> class SNode{
+  private:
+    SNode<T> * m_next;
+    T m_val;
+  public:
+    /*
+    Make explicit to prevent implicit conversions
+    */
+    explicit SNode(const T & val):
+    m_next{nullptr},
+    m_val{val}{
+    
+    }
+    
+    /*
+    Make explicit to prevent implicit conversions
+    */
+    explicit SNode(T && val):
+    m_next{nullptr},
+    m_val{std::move(val)}
+    {
+
+    }
+    
+    /* 
+    Disable copy constructor and copy assignment operator
+    */
+    SNode(const SNode<T> &) = delete;
+    SNode & operator=(const SNode<T> &) = delete;
+ 
+    /*
+    Disable move constructor and move assignment operator
+    */
+    SNode(SNode<T>&& other) = delete;
+    SNode& operator=(SNode<T>&& other) = delete;
+    
+    T & get_val() {
+      return m_val;
+    }
+
+    const T & get_val() const{
+      return m_val;
+    }
+
+    SNode<T> * get_next(){
+       return m_next;
+    }
+
+    const SNode<T> * get_next() const{
+       return m_next;
+    }
+  
+    void set_next(SNode<T> * next){
+       m_next = next;
+    }
+};
+
+#endif
