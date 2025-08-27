9073fab96c99dedeb9bb92d2e27574b9b26d8144
ekkapot.c@tggs.kmutnb.ac.th <ekkapot.c@tggs.kmutnb.ac.th>
2025-08-27 16:34:26 +0700

    minor upate

--- DataStructures/PriorityQueue/priority_queue.hpp
+++ DataStructures/PriorityQueue/priority_queue.hpp
@@ -49,9 +49,9 @@ public:
     assert(pos < size());
 
     if constexpr (std::is_same_v<Compare, std::greater<PQNode<S, T>>>) {
-	
+
       assert(new_key >= m_heap[pos].get_priority());
-	
+
       if (new_key >= m_heap[pos].get_priority()) {
         m_heap[pos].set_priority(new_key);
 
@@ -66,7 +66,7 @@ public:
         }
       }
     } else if constexpr (std::is_same_v<Compare, std::less<PQNode<S, T>>>) {
-      
+
       assert(new_key <= m_heap[pos].get_priority());
 
       if (new_key <= m_heap[pos].get_priority()) {
