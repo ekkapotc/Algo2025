fdf7eb1b01832696cb2eedf380b47e2c6d929f99
ekkapot.c@tggs.kmutnb.ac.th <ekkapot.c@tggs.kmutnb.ac.th>
2025-08-27 23:41:28 +0700

    update main.cpp

--- Sorting/HeapSort/main.cpp
+++ Sorting/HeapSort/main.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include "heap_sort.hpp"
 
 int main() {
@@ -6,12 +8,24 @@ int main() {
 
   heap_sort<int, std::less<int>> h_int{v};
 
-  auto sorted_data = h_int.sort();
+  auto sorted_data_int = h_int.sort();
 
-  std::cout << "Sorted: " << std::endl;
+  std::cout << "Sorted: (decreasing)" << std::endl;
 
   for (size_t i{0}; i < v.size(); i++) {
-    std::cout << sorted_data[i] << (i + 1 < v.size() ? "," : "\n");
+    std::cout << sorted_data_int[i] << (i + 1 < v.size() ? "," : "\n");
+  }
+
+  std::vector<std::string> w = {"string","int","char","bool","vector","float","map","double","stack","long"};
+
+  heap_sort<std::string> h_str{w};
+
+  auto sorted_data_str = h_str.sort();
+
+  std::cout << "Sorted: (increasing)" << std::endl;
+
+  for(size_t i{0}; i < w.size(); i++){
+     std::cout << sorted_data_str[i] << (i + 1 < w.size() ? "," : "\n");
   }
 
   return 0;
