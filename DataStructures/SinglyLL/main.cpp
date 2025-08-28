4e2fbb9162ead9d3ac36b593f78e9f1ba04bb273
ekkapot.c@tggs.kmutnb.ac.th <ekkapot.c@tggs.kmutnb.ac.th>
2025-08-28 15:11:42 +0700

    improve main

--- DataStructures/SinglyLL/main.cpp
+++ DataStructures/SinglyLL/main.cpp
@@ -4,27 +4,45 @@
 #include "singly_linked_list.hpp"
 
 int main() {
-  singly_linked_list<std::string> list;
+  singly_linked_list<std::string> l_str;
 
-  list.add("Alice");
-  list.add("Bob");
-  list.add("Eve");
-  list.add("Oscar");
-  list.add("Alice");
-  list.add("Mallory");
+  l_str.add("Alice");
+  l_str.add("Bob");
+  l_str.add("Eve");
+  l_str.add("Oscar");
+  l_str.add("Alice");
+  l_str.add("Mallory");
 
-  std::cout << list << std::endl;
+  std::cout << l_str << std::endl;
 
-  list.remove("Carol");
-  list.remove("Eve");
+  l_str.remove("Carol");
+  l_str.remove("Eve");
 
-  std::cout << list << std::endl;
+  std::cout << l_str << std::endl;
 
-  list.remove("Alice");
-  list.remove("Mallory");
-  list.remove("Oscar");
-  list.remove("Bob");
-  std::cout << list << std::endl;
+  l_str.remove("Alice");
+  l_str.remove("Mallory");
+  l_str.remove("Oscar");
+  l_str.remove("Bob");
+  std::cout << l_str << std::endl;
+
+  singly_linked_list<int> l_int;
+  
+  l_int.add(3);
+  l_int.add(10);
+  l_int.add(5);
+  l_int.add(2);
+  l_int.add(2);
+  l_int.add(10);
+  l_int.add(8);
+
+  std::cout << l_int << std::endl;
+
+  l_int.remove(3);
+  l_int.remove(8);
+  l_int.remove(10);
+
+  std::cout << l_int << std::endl;
 
   return 0;
 }
