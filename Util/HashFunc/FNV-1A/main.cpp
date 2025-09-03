89cf988ee3407fdb8bdb5051ca9865243f8edc17
ekkapot.c@tggs.kmutnb.ac.th <ekkapot.c@tggs.kmutnb.ac.th>
2025-09-03 19:47:00 +0700

    update main.cpp

--- Util/HashFunc/FNV-1A/main.cpp
+++ Util/HashFunc/FNV-1A/main.cpp
@@ -5,7 +5,7 @@
 
 int main() {
   std::string s1{"Hello World!"};
-  std::string s2{"Hellow Moon!"};
+  std::string s2{"Hello Moon!"};
   std::cout << fnv1a{s1.c_str(), s1.length()}() << std::endl;
   std::cout << fnv1a{s2.c_str(), s2.length()}() << std::endl;
   return 0;
