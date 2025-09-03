cb43943d16e91b98957d18e0d57a28a4741feb45
ekkapot.c@tggs.kmutnb.ac.th <ekkapot.c@tggs.kmutnb.ac.th>
2025-09-03 21:35:58 +0700

    Initial commit for insertion sort

---
+++ Sorting/InsertSort/insertion_sort.hpp
@@ -0,0 +1,63 @@
+#ifndef INSERTION_SORT_HPP_INC
+#define INSERTION_SORT_HPP_INC
+
+#include <cassert>
+#include <vector>
+#include <utility>
+
+template <typename T> class insertion_sort {
+private:
+  T *m_data;
+  size_t m_len;
+
+public:
+  insertion_sort(const T *arr, size_t len) : m_data{nullptr}, m_len{len} {
+    assert(m_len > 0);
+    m_data = new T[m_len];
+    for (size_t i{0}; i < m_len; i++)
+      m_data[i] = arr[i];
+  }
+
+  insertion_sort(std::vector<T> &vec) : m_data{nullptr}, m_len{vec.size()} {
+    assert(m_len > 0);
+    m_data = new T[m_len];
+    for (size_t i{0}; i < m_len; i++)
+      m_data[i] = vec[i];
+  }
+
+  insertion_sort(std::vector<T> &&vec) : m_data{nullptr}, m_len{vec.size()} {
+    assert(m_len > 0);
+    m_data = new T[m_len];
+    for (size_t i{0}; i < m_len; i++)
+      m_data[i] = std::move(vec[i]);
+  }
+
+  insertion_sort(const insertion_sort & other ) = delete;
+  
+  insertion_sort(insertion_sort &&other) = delete;
+
+  insertion_sort operator=(const insertion_sort &other) = delete;
+
+  insertion_sort operator=(insertion_sort &&other) = delete;
+
+  ~insertion_sort() { delete [] m_data; }
+
+  T * sort(){
+    if(m_data){
+      for(size_t j{1} ; j<m_len; j++){
+        T key = m_data[j];
+        size_t i = j;
+        while(i>0 && m_data[i-1]>key){
+	   m_data[i] = m_data[i-1];
+           i--;
+        }
+        m_data[i] = key;
+      }
+    }
+
+    return m_data;
+  }
+
+};
+
+#endif
