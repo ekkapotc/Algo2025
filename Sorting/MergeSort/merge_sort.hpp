396d21446c795d2a63a958408ee87b3824cd4e0c
ekkapot.c@tggs.kmutnb.ac.th <ekkapot.c@tggs.kmutnb.ac.th>
2025-09-12 13:56:50 +0700

    Use only a single array

--- Sorting/MergeSort/merge_sort.hpp
+++ Sorting/MergeSort/merge_sort.hpp
@@ -13,6 +13,7 @@ template <typename T> using DecOrd = std::less<T>;
 template <typename T, typename Compare = IncOrd<T>> class merge_sort {
 private:
   T *m_data;
+  T *m_temp;
   size_t m_len;
   Compare m_cmp;
 
@@ -30,65 +31,65 @@ private:
     size_t nl = q - p + 1;
     size_t nr = r - q;
 
-    T *al = new T[nl];
-    T *ar = new T[nr];
-
     for (size_t i = 0; i < nl; i++)
-      al[i] = m_data[p + i];
+      m_temp[p + i] = m_data[p + i];
     for (size_t i = 0; i < nr; i++)
-      ar[i] = m_data[i + q + 1];
+      m_temp[q + 1 + i] = m_data[q + 1 + i];
 
-    size_t lp = 0;
-    size_t rp = 0;
+    size_t lp = p;
+    size_t rp = q + 1;
     size_t kp = p;
 
-    while (lp < nl && rp < nr) {
-      if (m_cmp(ar[rp], al[lp])) {
-        m_data[kp] = std::move(al[lp]);
+    while (lp < p + nl && rp < q + 1 + nr) {
+      if (m_cmp(m_temp[rp], m_temp[lp])) {
+        m_data[kp] = std::move(m_temp[lp]);
         lp++;
       } else {
-        m_data[kp] = std::move(ar[rp]);
+        m_data[kp] = std::move(m_temp[rp]);
         rp++;
       }
       kp++;
     }
 
-    while (lp < nl) {
-      m_data[kp] = std::move(al[lp]);
+    while (lp < p + nl) {
+      m_data[kp] = std::move(m_temp[lp]);
       lp++;
       kp++;
     }
 
-    while (rp < nr) {
-      m_data[kp] = std::move(ar[rp]);
+    while (rp < q + 1 + nr) {
+      m_data[kp] = std::move(m_temp[rp]);
       rp++;
       kp++;
     }
-
-    delete[] ar;
-    delete[] al;
   }
 
 public:
-  merge_sort(const T *arr, size_t len) : m_data{nullptr}, m_len{len} {
+  merge_sort(const T *arr, size_t len)
+      : m_data{nullptr}, m_temp{nullptr}, m_len{len} {
     assert(m_len > 0);
     m_data = new T[m_len];
     for (size_t i{0}; i < m_len; i++)
       m_data[i] = arr[i];
+    m_temp = new T[m_len];
   }
 
-  merge_sort(std::vector<T> &vec) : m_data{nullptr}, m_len{vec.size()} {
+  merge_sort(std::vector<T> &vec)
+      : m_data{nullptr}, m_temp{nullptr}, m_len{vec.size()} {
     assert(m_len > 0);
     m_data = new T[m_len];
     for (size_t i{0}; i < m_len; i++)
       m_data[i] = vec[i];
+    m_temp = new T[m_len];
   }
 
-  merge_sort(std::vector<T> &&vec) : m_data{nullptr}, m_len{vec.size()} {
+  merge_sort(std::vector<T> &&vec)
+      : m_data{nullptr}, m_temp{nullptr}, m_len{vec.size()} {
     assert(m_len > 0);
     m_data = new T[m_len];
     for (size_t i{0}; i < m_len; i++)
       m_data[i] = std::move(vec[i]);
+    m_temp = new T[m_len];
   }
 
   merge_sort(const merge_sort &other) = delete;
@@ -99,7 +100,10 @@ public:
 
   merge_sort operator=(merge_sort &&other) = delete;
 
-  ~merge_sort() { delete[] m_data; }
+  ~merge_sort() {
+    delete[] m_temp;
+    delete[] m_data;
+  }
 
   T *sort() {
     if (m_data) {
